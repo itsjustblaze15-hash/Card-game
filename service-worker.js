799dae6285a5a5a45806f57483f9a87012dcbba3
itsjustblaze15-hash <itsjustblaze15@gmail.com>
2026-09-11 22:15:04 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="golden-pulls-v11-09-username-under-brand";
+const CACHE="golden-pulls-v11-10-asset-restore";
 self.addEventListener("install",()=>self.skipWaiting());
 self.addEventListener("activate",e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})()));
 self.addEventListener("message",e=>{if(e.data&&e.data.type==="SKIP_WAITING")self.skipWaiting()});
